b6383c5525894290258b9c764da0c55e3b256bfc
Noah Nichols <noahjacknichols@gmail.com>
2020-09-11 19:13:30 -0400

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
 import Portfolio from './Components/Portfolio';
 import Intro from './Components/Intro';
 import Contact from './Components/Contact';
 import Skills from './Components/Skills';
-import {Container} from 'react-bootstrap';
 
 
 function App() {
   return ( 
       <div>
-        {/* <div className="Yellow">
-          <Navbar/>
-        </div> */}
 
         <div className="Yellow">
             <br/>
